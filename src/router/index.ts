185faf3aceb09fb62a0ecb898b363d5b46aeeb11
longpv <longpv@vivas.vn>
2025-07-04 22:13:26 +0700

    dev

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,58 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import DataSources from '../views/DataSources.vue'
-import QuickDashboard from '../views/QuickDashboard.vue'
-import DashboardStore from '../views/DashboardStore.vue'
-import TemplateDesigner from '../views/TemplateDesigner.vue'
-import JournalComponent from '../views/JournalComponent.vue'
-import CsvTester from '../views/CsvTester.vue'
-import AI from '../views/AI.vue'
+import instruct from '../views/instruct.vue'
+
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
-  },
-  {
-    path: '/data-sources',
-    name: 'DataSources',
-    component: DataSources
-  },
-  {
-    path: '/dashboards',
-    name: 'DashboardStore',
-    component: DashboardStore
-  },
-  {
-    path: '/dashboard-store',
-    name: 'DashboardStoreAlias',
-    component: DashboardStore
-  },
-  {
-    path: '/quick-dashboard',
-    name: 'QuickDashboard',
-    component: QuickDashboard
-  },
-  {
-    path: '/template-designer',
-    name: 'TemplateDesigner',
-    component: TemplateDesigner
-  },
-  {
-    path: '/diary',
-    name: 'diary',
-    component: JournalComponent
-  },
-  {
-    path:'/csv-tester',
-    name: 'CsvTester',
-    component:CsvTester
-  },
-   {
-    path:'/AI',
-    name: 'AI',
-    component:AI
-  },
+    component: instruct
+  }
+
 ]
 
 export const router = createRouter({
